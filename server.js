2cc8a293a53ae580cae82f900c60b9a011fe6cf4
Gregor Ojstersek <grega.ojstersek@gmail.com>
2018-09-22 18:29:01 +0200

    commit changes for heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const request = require('request');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.static(__dirname + '/view'));
@@ -33,4 +35,6 @@ app.get('/recipes', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
